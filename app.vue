25949847823ad159f7c674344c80711d504ab16c
Marin <marin.knezevic1et2@gmail.com>
2021-11-07 19:47:44 +0100

    trying something

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Fetching data with api</h1>
+    <PopularMovies />
+  </div>
+</template>
+<script>
+import PopularMovies from "/components/PopularMovies";
+export default {
+  name: "App",
+  components: {
+    PopularMovies,
+  },
+};
+</script>
