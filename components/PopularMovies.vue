7425aa11003aa024c99bc60c5c187a87b3921e59
Marin <marin.knezevic1et2@gmail.com>
2021-11-07 21:08:52 +0100

    something went wrong

--- components/PopularMovies.vue
+++ components/PopularMovies.vue
@@ -4,7 +4,19 @@
   </div>
 </template>
 <script>
+import Vue from 'vue';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios,axios)
+
 export default {
   name: "PopularMovies",
+  mounted()
+  {
+    Vue.axios.get('https://www.themoviedb.org/movie')
+    .then((response)=>{
+      console.warn(response)
+    })
+  }
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,27 @@
 <template>
-<h1>Hi</h1>
-  
+  <div id="app">
+    <h1>Fetching data with api</h1>
+    <PopularMovies />
+  </div>
 </template>
-
 <script>
-export default {};
+import PopularMovies from "/components/PopularMovies";
+export default {
+  name: "App",
+  components: {
+    PopularMovies,
+  },
+};
 </script>
+
+<style>
+#app{
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    font-size: 2rem;
+    margin-top: 60px;
+}
+</style>
